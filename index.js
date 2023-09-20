82f4271085a96ad7b8404282c4b0924382a637ab
Paola Roberta Lalli <robyfranky@hotmail.com>
2023-09-20 15:06:19 +0200

    first iteration

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // Iteration 1: Names and Input
-
+const hacker1 = "Monica";
+console.log(`The driver's name is ${hacker1}`);
+const hacker2 = "Roberta";
+console.log(`The navigator name is ${hacker2}`);
 
 // Iteration 2: Conditionals
-
 
 // Iteration 3: Loops
